ce493aad55e05af77f43bdb8e21c4bbb673a7023
pingchang.ke <kpc@vincross.com>
2017-12-19 19:02:20 +0800

    fix all.

--- OpenGLStudy/RotaMapView.swift
+++ OpenGLStudy/RotaMapView.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class RotaMapView: UIView {
     
-    var dataArr = Array<Array<Int>>()
+    var dataArr = Array<JSON>()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -24,9 +24,27 @@ class RotaMapView: UIView {
     
     override func draw(_ rect: CGRect) {
         let context = UIGraphicsGetCurrentContext()
-        for i in 0..<dataArr.count {
-            for j in 0..<dataArr[i].count {
-                if dataArr[i][j] > 0 {
+        
+        let width = Int(bounds.width)
+        let height = Int(bounds.height)
+
+        
+        var newArr = Array(repeating:Array(repeating:Int(), count:width), count:height)
+
+        
+                for i in 0..<height {
+                    for j in 0..<width {
+                        newArr[i][j] = dataArr[i*width + j].intValue
+                    }
+                }
+        
+        
+        print(newArr)
+
+        
+        for i in 0..<newArr.count {
+            for j in 0..<newArr[i].count {
+                if newArr[i][j] > 0 {
                     CGContext.addRect(context!)(CGRect(x: j, y: i, width: 1, height: 1))
                 }
             }
